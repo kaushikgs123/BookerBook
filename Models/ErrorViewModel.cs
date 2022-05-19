89d56e5615ae5467d00110ff4351cb3484db3072
kaushikgs <kaushikgs@DESKTOP-9PPGFK4>
2022-05-19 18:30:38 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookerBook.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
